86a539b8f12c943dd39ba8041bb25f0f781fa1ac
Joshua <jk7045@cs.ship.edu>
2021-09-20 23:01:29 -0400

    Actual Initial Commit

---
+++ War/Assets/Scripts/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+ 
+     private CharacterController controller;
+     public float speed = 10f;
+     public Vector3 targetPos;
+     private Vector3 playerVelocity;
+     private float gravityValue = 9.81f;
+     public bool isMoving;
+     private bool groundedPlayer;
+     const int MOUSE = 0;
+     // Use this for initialization1
+     void Start () {
+         //controller = gameObject.AddComponent<CharacterController>();
+         targetPos = transform.position;
+         isMoving = false;
+     }
+     
+     // Update is called once per frame
+     void Update () {
+        
+        // if (controller.isGrounded == false)
+        // {
+        //     targetPos.y += gravityValue * Time.deltaTime;
+        //     MoveObject();
+        // }
+         if(Input.GetMouseButton(MOUSE))
+         {
+             SetTargetPosition();
+         }
+         if(isMoving)
+         {
+             MoveObject();
+         }
+     }
+     void SetTargetPosition()
+     {
+         Plane plane = new Plane(Vector3.up,transform.position);
+         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+         float point = 0f;
+ 
+         if(plane.Raycast(ray, out point))
+             targetPos = ray.GetPoint(point);
+         
+         isMoving = true;
+     }
+     void MoveObject()
+     {
+         transform.LookAt(targetPos);
+         transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+ 
+         //targetPos.y += gravityValue * Time.deltaTime;
+         //playerVelocity.y += gravityValue * Time.deltaTime;
+        // transform.position = Vector3.MoveTowards(transform.position, playerVelocity, gravityValue * Time.deltaTime);
+         //controller.Move(playerVelocity * Time.deltaTime);
+
+         if (transform.position == targetPos)
+             isMoving = false;
+         Debug.DrawLine(transform.position,targetPos,Color.red);
+ 
+     }
+}
